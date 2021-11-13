f769d558b24d4a260642eb6740513cfb9a46628d
shayanafkane <shayanafkane@gmail.com>
2021-11-13 16:26:04 +0330

    Add-disabled-button

--- src/common/form.jsx
+++ src/common/form.jsx
@@ -2,24 +2,26 @@ import axios from 'axios';
 import React from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-
 class Form extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
             tel: '',
+            disabled: ''
         }
-
+        this.setState({
+            disabled: false
+        })
     }
     handleHtmlControlChange = (event) => {
         this.setState({ [event.target.name]: event.target.value })
     }
-    handleSubmit = (event) => {
 
+    handleSubmit = (event) => {
+        event.preventDefault();
         let formData = new FormData();
         formData.append("tel", this.state.tel);
-        event.preventDefault();
         const config = {
             headers: { "Content-Type": "application/x-www-form-urlencoded" }
         }
@@ -28,31 +30,37 @@ class Form extends React.Component {
             .then((response) => {
 
                 console.log(response.data);
+
                 if (response.data.result == true) {
                     toast.success("اطلاعات شما با موفقیت ثبت شد");
+                    this.state.disabled = false;
 
                 } else if (response.data.result == false) {
                     toast.error("شماره موبایل اشتباه وارد شده است");
+                    this.state.disabled = false;
                 } else if (response.data.result == "duplicate") {
                     toast.error("شماره موبایل وارد شده تکراری است");
+                    this.state.disabled = false;
                 }
-            })
 
+            })
+        const tel = this.state.tel;
+        this.setState({
+            tel: ''
+        });
     }
     render() {
         const { tel } = this.state;
-
         return (
             <div className="box-form-blackfriday">
                 <h3> برای اینکه زودتر از همه از تخفیف ها با خبر شید فرم زیر را پر کنید !</h3>
                 <form action="#" className="form-black" onSubmit={this.handleSubmit}>
                     <input type="text" value={tel} className="form-control" onChange={this.handleHtmlControlChange} placeholder="شماره تماس خودرا وارد کنید" name="tel" />
-                    <input type="submit" className="btn btn-danger" value="ارسال اطلاعات" />
+                    <input type="submit" className="btn btn-danger" disabled={this.state.disabled} onClick={() => this.state.disabled = true} value="ارسال اطلاعات" />
                 </form>
-
                 <ToastContainer />
-
             </div>
+
         )
     }
 }
